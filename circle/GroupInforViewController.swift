1f43bb4cce0e9822ed1ae95653d403131a6f95dd
Marquavious Draggon <marq.draggon@gmail.com>
2016-09-10 20:44:04 -0700

    info controller

---
+++ circle/GroupInforViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GroupInforViewController.swift
+//  circle
+//
+//  Created by Marquavious on 9/10/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import UIKit
+
+class GroupInforViewController: ViewController {
+
+}
