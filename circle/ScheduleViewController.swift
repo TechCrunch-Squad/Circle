609c636679bb4e9724db0d7a4af28e52240131c1
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-11 01:47:48 -0700

    Update constrain

---
+++ circle/ScheduleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ScheduleViewController.swift
+//  circle
+//
+//  Created by Zulwiyoza Putra on 9/11/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScheduleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
